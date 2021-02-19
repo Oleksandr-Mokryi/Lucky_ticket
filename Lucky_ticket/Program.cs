ca4cbdf3225160983707bf0e2f8b5d6e17f8dc83
Oleksandr Mokryi <montegero@gmail.com>
2021-02-19 14:23:00 +0200

    initial commit

---
+++ Lucky_ticket/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Lucky_ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your ticket number");
+            while (true)
+            {
+                string text = Console.ReadLine();
+                bool isInt = int.TryParse(text, out int mark);
+                if (!isInt)
+                {
+                    Console.WriteLine("No ticket number is found. Please try again");
+                    continue;
+                }
+                if (text.Length < 4 || text.Length > 8)
+                {
+                    Console.WriteLine("Wrong ticket number. Please try again");
+                    continue;
+                }
+                if (text.Length % 2 != 0)
+                {
+                    text = '0' + text;
+                }
+                int[] digits = text.Select(n => (n - '0')).ToArray();
+                int half_1 = 0;
+                int half_2 = 0;
+                for(int i = 0; i < digits.Length / 2; i++)
+                {
+                    half_1 += digits[i];
+                }
+                for (int i = digits.Length / 2; i < digits.Length; i++) 
+                {
+                    half_2 += digits[i];
+                }
+                if (half_1 == half_2)
+                {
+                    Console.WriteLine("Lucky one!");
+                }
+                else
+                {
+                    Console.WriteLine("Better luck next time");
+                }
+            }
+        }
+    }
+}
